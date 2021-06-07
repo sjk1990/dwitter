e9bb3bd02ec0d44ca8547598034315c038f34de9
SJKim <worldsjk1990@gmail.com>
2021-06-07 22:47:30 +0900

    .env config setting

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import * as userRepository from "../data/auth.js";
+import { config } from "../config.js";
 
 const AUTH_ERROR = { message: "Authentication Error" };
 
@@ -11,19 +12,15 @@ export const isAuth = async (req, res, next) => {
 
   const token = authHeader.split(" ")[1];
   // TODO: Make it secure!
-  jwt.verify(
-    token,
-    "F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z",
-    async (error, decoded) => {
-      if (error) {
-        return res.status(401).json(AUTH_ERROR);
-      }
-      const user = await userRepository.findById(decoded.id);
-      if (!user) {
-        return res.status(401).json(AUTH_ERROR);
-      }
-      req.userId = user.id; // req.customData
-      next();
+  jwt.verify(token, config.jwt.secretKey, async (error, decoded) => {
+    if (error) {
+      return res.status(401).json(AUTH_ERROR);
     }
-  );
+    const user = await userRepository.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json(AUTH_ERROR);
+    }
+    req.userId = user.id; // req.customData
+    next();
+  });
 };
