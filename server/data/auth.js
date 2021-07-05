269f8d27ae6432d7295bd38959ac9f8e202af9b4
SJKim <worldsjk1990@gmail.com>
2021-07-05 23:04:56 +0900

    data/auth - mongodb

--- server/data/auth.js
+++ server/data/auth.js
@@ -1,32 +1,26 @@
-// abcd1234: $2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm
-let users = [
-  {
-    id: "1",
-    username: "bob",
-    password: "$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm",
-    name: "Bob",
-    email: "bob@gmail.com",
-    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
-  },
-  {
-    id: "2",
-    username: "ellie",
-    password: "$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm",
-    name: "Ellie",
-    email: "ellie@gmail.com",
-  },
-];
+import MongoDB from "mongodb";
+import { getUsers } from "../database/database.js";
+const ObjectID = MongoDB.ObjectID;
 
+// SQL: DB Schema
+// NOSQL: DB Schema X, ORM Schema
 export async function findByUsername(username) {
-  return users.find((user) => user.username === username);
+  return getUsers().find({ username }).next().then(mapOptionalUser);
 }
 
 export async function findById(id) {
-  return users.find((user) => user.id === id);
+  return getUsers()
+    .find({ _id: new ObjectID(id) })
+    .next()
+    .then(mapOptionalUser);
 }
 
 export async function createUser(user) {
-  const created = { ...user, id: Date.now().toString() };
-  users.push(created);
-  return created.id;
+  return getUsers()
+    .insertOne(user)
+    .then((result) => result.ops[0]._id.toString());
+}
+
+function mapOptionalUser(user) {
+  return user ? { ...user, id: user._id.toString() } : user;
 }
--- server/database/database.js
+++ server/database/database.js
@@ -6,5 +6,15 @@ export async function connectDB() {
   return MongoDB.MongoClient.connect(config.db.host, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+  }).then((client) => {
+    db = client.db();
   });
+}
+
+export function getUsers() {
+  return db.collection("users");
+}
+
+export function getTweets() {
+  return db.collection("tweets");
 }
